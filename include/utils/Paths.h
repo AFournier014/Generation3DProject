a159aeb471b86ffa874616eb116bed98f4901f0a
Alex Fournier <57791801+AFournier014@users.noreply.github.com>
2024-04-14 16:48:45 +0200

    Modification des paths pour les rendres plus simple, ne sera pas maintenable en l'état

--- include/utils/Paths.h
+++ include/utils/Paths.h
@@ -2,17 +2,5 @@
 
 #include <string>
 
-//const std::string SHADER_PATH = "../../ressources/shaders/";
-
-#if defined(DEBUG) || defined(_DEBUG)
-// Debug Mode
 static const std::string SHADER_PATH = "../ressources/shaders/";
-
-#elif defined(RELEASE) || defined(NDEBUG)
-// Release Mode
-static const std::string SHADER_PATH = "../ressources/shaders/";
-
-#else
-// Fallback ou mode par défaut, peut-être pour d'autres configurations ou l'exécutable
-static const std::string SHADER_PATH = "shaders/";
-#endif
+static const std::string ASSETS_PATH = "../ressources/assets/";
