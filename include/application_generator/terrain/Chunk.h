154d73a651df80494d8ee52f4d2c27f60e1c229e
vgilabert <vincent.gilabert@gmail.com>
2024-05-16 14:40:45 +0200

    WIP
    Change heightMap generation method + add params

--- include/application_generator/terrain/Chunk.h
+++ include/application_generator/terrain/Chunk.h
@@ -3,38 +3,65 @@
 
 #include "Meshs/Mesh.h"
 
-struct RenderConfig;
+struct HeightMap
+{
+    std::vector<std::vector<float>> values;
+    float minValue;
+    float maxValue;
+
+    HeightMap(const std::vector<std::vector<float>>& values, float minValue, float maxValue)
+        : values(values), minValue(minValue), maxValue(maxValue)
+    {
+    }
+};
 
 class Chunk
 {
 public:
-	Chunk(int size, const std::shared_ptr<RenderConfig>& renderConfig);
-	Mesh* getMesh() const { return m_mesh; }
-
+    Chunk(int size, const Vector3D<float>& position);
+    Mesh* getMesh() const { return m_mesh; }
+    
+    bool& get_auto_update() { return auto_update; }
+    float& get_height_multiplier() { return heightMultiplier; }
+    float& get_scale() { return scale; }
+    int& get_octaves() { return octaves; }
+    float& get_persistance() { return persistance; }
+    float& get_lacunarity() { return lacunarity; }
+    
 private:
-	void generate_mesh(int chunkSize, Vector3D<float> pos);
-	static void recalculate_normals(std::vector<Vertexf>& vertices, const std::vector<unsigned int>& indices);
-	static std::vector<std::vector<float>> generate_height_map(int width, int height);
-	
-	Mesh* m_mesh;
-	int m_size;
-	Vector3D<float> m_position;
-	std::shared_ptr<RenderConfig> m_renderConfig;
+    void generate_mesh(int chunkSize, Vector3D<float> pos);
+    static void recalculate_normals(std::vector<Vertexf>& vertices, const std::vector<unsigned int>& indices);
+    static HeightMap generate_height_map(int width, int height, float heightMultiplier, float scale, int octaves, float persistance, float lacunarity);
+    static std::vector<std::vector<float>> generate_noise_map(int width, int height, float scale, int octaves,
+                                                              float persistance, float lacunarity);
+
+    Mesh* m_mesh;
+    int m_size;
+    Vector3D<float> m_position;
+
+#pragma region TerrainSettings
+    bool auto_update = true;
+    float heightMultiplier = 25;
+    float scale = 50;
+    int octaves = 8;
+    float persistance = 0.5f;
+    float lacunarity = 1.5f;
+#pragma endregion // TerrainSettings
 };
 
 inline float MapValue(float minimum, float maximum, float value)
 {
-	return minimum + (maximum - minimum) * ((value - 0) / (1 - 0));
+    return minimum + (maximum - minimum) * ((value - 0) / (1 - 0));
 }
 
 inline float GetRandF(float fMin, float fMax)
 {
-	return (fMax - fMin) * ((((float)rand()) / (float)RAND_MAX)) + fMin;
+    return (fMax - fMin) * ((((float)rand()) / (float)RAND_MAX)) + fMin;
 }
 
 inline int GetRand(int min, int max)
 {
-	return min + (rand() % static_cast<int>(max - min + 1));
+    return min + (rand() % static_cast<int>(max - min + 1));
 }
 
 #endif // APPLICATION_H
--- src/application_generator/terrain/Chunk.cpp
+++ src/application_generator/terrain/Chunk.cpp
@@ -1,5 +1,7 @@
 #include "terrain/Chunk.h"
 
+#include <iostream>
+
 #include "Config.h"
 #include "terrain/PerlinNoise.h"
 
@@ -14,21 +16,12 @@ void Chunk::generate_mesh(int chunkSize, Vector3D<float> pos)
     std::vector<Vertexf> vertices;
     std::vector<unsigned int> indices;
 	  auto texture = m_renderConfig->texture;
+    std::vector<std::vector<float>> height_map = generate_height_map(chunkSize + 1, chunkSize + 1, heightMultiplier, scale, octaves,
+                                                                    persistance, lacunarity).values;
 
-
-    PerlinNoise pn(static_cast<unsigned int>(time(nullptr)));
-    double xOff = 0.0;
-    double zOff = 0.0;
-    double min = 0;
-    double max = GetRand(static_cast<int>(min) + 1, 25);
-    double noiseValue = 0.0;
-    float modifier = GetRandF(0.01f, 0.08f);
-
-    
     Color3<float> color = {1, 1, 1};
     for (int i = 0; i <= chunkSize; ++i)
     {
-        xOff = 0.0;
         for (int j = 0; j <= chunkSize; ++j)
         {
             // TODO: fix colors
@@ -37,17 +30,15 @@ void Chunk::generate_mesh(int chunkSize, Vector3D<float> pos)
                     static_cast<float>(rand()) / RAND_MAX, static_cast<float>(rand()) / RAND_MAX,
                     static_cast<float>(rand()) / RAND_MAX
                 };
-            noiseValue = pn.noise(xOff, 0, zOff);
-            float height = MapValue(static_cast<float>(min), static_cast<float>(max), static_cast<float>(noiseValue));
+            float height = height_map[i][j];
+            // TODO: remove log
+            // std::cout << "Height: " << height << std::endl;
             Vertexf vertex = {
-                {pos.x() + i, pos.y() + height, pos.z() + j}, {0, 1, 0}, color,
+                {pos.x() + i, pos.y() + height, pos.z() - j}, {0, 1, 0}, color,
                 {0, 0}
             };
             vertices.push_back(vertex);
-            
-            xOff += modifier;
         }
-        zOff += modifier;
     }
 
     for (int i = 0; i < chunkSize; ++i)
@@ -91,4 +82,93 @@ void Chunk::recalculate_normals(std::vector<Vertexf>& vertices, const std::vecto
         vertices[indices[i + 1]].normal = normal;
         vertices[indices[i + 2]].normal = normal;
     }
+}
+
+HeightMap Chunk::generate_height_map(int width, int height, float heightMultiplier, float scale, int octaves,
+                                   float persistance, float lacunarity)
+{
+    std::vector<std::vector<float>> values = Chunk::generate_noise_map(width, height, scale, octaves,
+                                                                       persistance, lacunarity);
+
+    float minValue = std::numeric_limits<float>::max();
+    float maxValue = std::numeric_limits<float>::min();
+
+    for (int i = 0; i < width; ++i)
+    {
+        for (int j = 0; j < height; ++j)
+        {
+            values[i][j] *= heightMultiplier;
+
+            minValue = std::min(minValue, values[i][j]);
+            maxValue = std::max(maxValue, values[i][j]);
+        }
+    }
+
+    return HeightMap(values, minValue, maxValue);
+}
+
+std::vector<std::vector<float>> Chunk::generate_noise_map(int width, int height, float scale, int octaves,
+                                                          float persistance,
+                                                          float lacunarity)
+{
+    PerlinNoise pn(static_cast<unsigned int>(time(nullptr)));
+    std::vector<std::vector<float>> noiseMap(width, std::vector<float>(height));
+    std::vector<Vector2f> octaveOffsets(octaves);
+
+    float maxPossibleHeight = 0;
+    float amplitude = 1;
+    float frequency = 1;
+
+    for (int i = 0; i < octaves; i++)
+    {
+        float offsetX = static_cast<float>(GetRand(-100000, 100000));
+        float offsetY = static_cast<float>(GetRand(-100000, 100000));
+        octaveOffsets[i] = {offsetX, offsetY};
+
+        maxPossibleHeight += amplitude;
+        amplitude *= persistance;
+    }
+
+    float maxLocalNoiseHeight = std::numeric_limits<float>::min();
+    float minLocalNoiseHeight = std::numeric_limits<float>::max();
+
+    float halfWidth = static_cast<float>(width) / 2.f;
+    float halfHeight = static_cast<float>(height) / 2.f;
+
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            amplitude = 1;
+            frequency = 1;
+            float noiseHeight = 0;
+
+            for (int i = 0; i < octaves; i++)
+            {
+                float sampleX = (x - halfWidth + octaveOffsets[i].x) / scale * frequency;
+                float sampleY = (y - halfHeight + octaveOffsets[i].y) / scale * frequency;
+
+                float perlinValue = static_cast<float>(pn.noise(sampleX, sampleY, 0) * 2 - 1);
+
+                noiseHeight += perlinValue * amplitude;
+
+                amplitude *= persistance;
+                frequency *= lacunarity;
+            }
+
+            if (noiseHeight > maxLocalNoiseHeight)
+            {
+                maxLocalNoiseHeight = noiseHeight;
+            }
+
+            if (noiseHeight < minLocalNoiseHeight)
+            {
+                minLocalNoiseHeight = noiseHeight;
+            }
+
+            noiseMap[x][y] = noiseHeight;
+        }
+    }
+
+    return noiseMap;
 }
